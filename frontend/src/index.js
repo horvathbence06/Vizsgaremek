55bee483f0b4810eaee1b7d43956fa5700672c1c
Milko020 <20d_kovacsm@nyirszikszi.hu>
2025-04-11 20:12:17 +0200

    Dokumentáció - Technikai modellek

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,8 +13,6 @@ root.render(
 );
 
 reportWebVitals();
-
-import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:8080';
 
